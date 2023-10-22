bdc5a5fad681816cec82cea22f750db30a8fb80d
Hassan <malikhassanhu55@gmail.com>
2023-10-22 23:28:28 +0500

    Weather

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import { useQuery } from '@tanstack/react-query';
+import Spinner from './Components/Spinner';
 
 
 function App() {
   let cityName = localStorage.getItem("city")
-  const { data } = useQuery({
+  const { data , isLoading } = useQuery({
     queryKey: ['weather'],
     queryFn: () =>
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=5555e631eb25d2ee80e48fc82a3ae893`).then(
         (res) => res.json(),
       ),
   })
+
+  if(isLoading){
+    return <Spinner/>
+  }
 
 console.log(data);
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,12 +6,12 @@ import { GiPressureCooker } from 'react-icons/gi'
 
 const Home = (props) => {
   let weatherData = props.data;
-  console.log(weatherData);
+ 
 
   let weatherDetails = [
     {
-      name: "Current Temperature",
-      temp: `${weatherData?.main.temp}°C`,
+      name: "Feels Like",
+      temp: `${weatherData?.main.feels_like}°C`,
       icon: <LiaCloudSolid />
     },
     {
@@ -35,14 +35,15 @@ const Home = (props) => {
       <div className='pb-12 pl-10' style={{ backgroundImage: `url("https://img.freepik.com/free-vector/gorgeous-clouds-background-with-blue-sky-design_1017-25501.jpg") `, backgroundSize: "100% 500px", backgroundRepeat: "no-repeat", width: "100%", height: "400px", borderRadius: "12px" }}>
 
         <p className='font-semibold ,  text-3xl md:text-6xl pt-12 '>
-          <span>{weatherData?.name} , </span>
+          <span>{weatherData?.name} , {weatherData?.sys.country} </span>
         </p>
-        <p className='font-semibold , text-4xl pt-5 '>
+        <p className='font-semibold flex  items-center text-4xl pt-5 '>
           <span>{weatherData?.main.temp}°C</span>
-          <span className='ml-10'>{weatherData?.weather[0].main}</span>
+          <span className='ml-10'>{weatherData?.weather[0].main==="Smoke"?<img className='w-36 h-32' src='https://o.remove.bg/downloads/30051fec-8606-40a7-9724-96ba0971890c/images-removebg-preview.png' alt='' />:"Smoke" || weatherData?.weather[0].main==="Clear"?<img className='w-36 h-32' src='https://o.remove.bg/downloads/d5d37c2b-0911-43f0-b172-870cd970f2d0/images-removebg-preview.png' alt='' />:"Clear" || weatherData?.weather[0].main==="Rain"?<img className='w-36 h-32' src='https://o.remove.bg/downloads/508c37a3-967b-4b06-ae63-13be4803a640/images-removebg-preview.png' alt='' />:"Rain" || weatherData?.weather[0].main==="Snow"?<img className='w-36 h-32' src='https://o.remove.bg/downloads/9a680ce6-339f-4807-8015-5f1a01b242a7/images-removebg-preview.png' alt='' />:"Snow" || weatherData?.weather[0].main==="Clouds"?<img className='w-36 h-32' src='https://o.remove.bg/downloads/dba1ee46-3ac8-4c90-886b-a5070eae8f44/images-removebg-preview.png' alt='' />:"Clouds"}</span>
+         
         </p>
         {/* <p className='mt-4 text-2xl '>Local Time: {weatherData?.location.localtime}</p> */}
-        <p className='mt-4 text-2xl '>Feels Like: {weatherData?.main.feels_like}°C</p>
+        {/* <p className='mt-4 text-2xl '>Feels Like: {weatherData?.main.feels_like}°C</p> */}
       </div>
 
       <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-3  '>
